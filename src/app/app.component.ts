dad686dfa5385d336871ad1fd85e4ce21d7f42bf
Mohamad Farhoud <Mohamadfarhoud@hotmail.com>
2024-09-07 20:49:13 +0200

    Exercise 4: Setup lazy-loading for User module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterModule, RouterOutlet } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { UserModule } from './user/user.module';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterModule, HomeComponent, PageNotFoundComponent],
+  imports: [RouterOutlet, RouterModule, HomeComponent, PageNotFoundComponent, UserModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,7 +5,12 @@ import { PageNotFoundComponent } from './page-not-found/page-not-found.component
 export const routes: Routes = [{
     path: '',
     component: HomeComponent
-}, {
+},
+{
+    path: 'users',
+    loadChildren: () => import('./user/user.module').then(m => m.UserModule)
+},
+{
     path: '**',
     component: PageNotFoundComponent
 },
